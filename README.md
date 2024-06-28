73ebf0cd7257f65001c8aa935cf9bd9940defd65
David Read <david.read@digital.justice.gov.uk>
2024-06-28 13:44:18 +0100

    First version of docx2ruletxt

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # OPA Tools
 
+A selection of tools for working with OPA (Oracle Policy Automation) rulebases.
+
 ## Setup
 
 ```
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+```
+
+Get the OPA rulebase, in the original Word & Excel docs:
+```sh
+cd ~/code
+git clone git@github.com:ministryofjustice/laa-ccms-opa-policy-models.git
+# If you've previously done this, move aside the old rules directory
+mv ~/code/laa-ccms-opa-policy-models-zips-extracted /tmp/
+unzip laa-ccms-opa-policy-models/MeansAssessment.zip -d ~/code/laa-ccms-opa-policy-models-zips-extracted
+```
+
+Clone this repository, which is where the resulting markdown goes
+```sh
+cd ~/code
+git clone git@github.com:ministryofjustice/laa-ccms-opa-means-assessment-markdown
 ```
 
 ## docx2ruletxt
@@ -18,6 +35,47 @@ To view a .docx file as .ruletxt:
 ```
 source venv/bin/activate
 python docx2ruletxt.py ../laa-ccms-opa-policy-models-zips-extracted/MeansAssessment/Rules/LAR/LAR\ System\ Rules.docx
+```
+### Convert all .docx files
+
+Locate the folders for the extracted zip and output repo (adjust these if you put them elsewhere):
+
+```sh
+export OPA_REPO_DIR=~/code/laa-ccms-opa-policy-models
+export EXTRACTED_MODELS_DIR=~/code/laa-ccms-opa-policy-models-zips-extracted
+export OUT_DIR=~/code/laa-ccms-opa-means-assessment-ruletxt
+export PYTHON_CMD=~/code/opa_tools/venv/bin/python
+export PARSE_PY=~/code/opa_tools/docx2ruletxt.py
+```
+
+Get the latest the Word docs:
+```sh
+cd $OPA_REPO_DIR
+git pull
+# If you've previously done this, move aside the old rules directory
+mv $EXTRACTED_MODELS_DIR /tmp/
+unzip MeansAssessment.zip -d $EXTRACTED_MODELS_DIR
+```
+
+Convert the Word docs:
+
+```sh
+cd $EXTRACTED_MODELS_DIR/MeansAssessment/rules
+find . -name "*.docx" -exec sh -c '
+  FILE="$1"
+  OUT_DIR="$2"
+  DIRNAME=$(dirname "$FILE")
+  BASENAME=$(basename "$FILE" .docx)
+  NEWNAME="${BASENAME// /_}.ruletxt"
+  DEST_DIR="$OUT_DIR/$DIRNAME"
+  mkdir -p "$DEST_DIR"
+  $PYTHON_CMD $PARSE_PY "$FILE" "$DEST_DIR/$NEWNAME" || echo "$FILE"
+' sh {} "$OUT_DIR" \;
+```
+
+Commit the changes to this repo:
+```sh
+cd $OUT_DIR
 ```
 
 
@@ -40,8 +98,8 @@ Locate the folders for the extracted zip and markdown repo (adjust these if you 
 ```sh
 export IN_DIR=~/code/laa-ccms-opa-policy-models-zips-extracted/MeansAssessment/rules
 export OUT_DIR=~/code/laa-ccms-opa-means-assessment-python
-export PYTHON_CMD=~/code/opa_analysis/venv/bin/python
-export PARSE_PY=~/code/opa_analysis/parse_docx.py
+export PYTHON_CMD=~/code/opa_tools/venv/bin/python
+export PARSE_PY=~/code/opa_tools/parse_docx.py
 ```
 
 Convert the Word docs to python:
--- docx2ruletxt.py
+++ docx2ruletxt.py
@@ -7,54 +7,85 @@ import docx
 #input_filepath = os.path.expanduser("~/code/laa-ccms-opa-policy-models-zips-extracted/MeansAssessment/Rules/LAR/LAR System Rules.docx")
 if len(sys.argv) > 1:
     input_filepath = sys.argv[1]
+    if os.path.basename(input_filepath).startswith('~$'):
+        print(f'Ignoring tempfile created by Word {input_filepath}')
+        sys.exit(0)
 else:
     print("No input filepath provided")
     sys.exit(1)
+print(input_filepath)
 
+f = None
 if len(sys.argv) > 2:
     out_filepath = sys.argv[2]
     f = open(out_filepath, 'w')
-    sys.stdout = f
 
 def print_(header_tag, body):
-    padding = ' ' * (max(len('[OPM-conclusion]') - len(header_tag) - 2, 0))
+    padding = ' ' * (max(len('[table-OPM-conclusion]') - len(header_tag) - 2, 0))
+    body = body.rstrip()
     if '\n' in body:
-        breakpoint()
-    print(f'[{header_tag}] {padding} {body}')
+        # very rare to see a newline inside a paragraph or table cell.
+        # print a symbol, rather than have lines without a header tag
+        body = body.replace('\n', '\\n')
+    line = f'[{header_tag}] {padding} {body}'
+    if f:
+        print(line, file=f)
+    else:
+        print(line)    
+
 
 def process_paragraph(p, doc_index):
     style = p.style.style_id
     indent = 0
     if style.startswith('OPM-level'):
         indent = int(style.split('OPM-level')[1])
-    print_(style, '    '*indent + f'{p.text}')
+    print_(style, '    ' * (indent - 1)+ f'{p.text}')
 
-def parse_table(table, doc_index):
+def process_table(table, doc_index):
     for row_index, r in enumerate(table.rows):
         row_cells = tuple(c for c in r.cells)
+
+        if row_index == 0 and row_cells[0].paragraphs[0].style.style_id != 'OPM-conclusion':
+            # this table isn't a rule
+            return process_nonrule_table(table, doc_index)
+        
+        # hack to fix one table in 'Work Package 3/WP3 Calculation 30 -37.docx'
+        if len(row_cells) == 3 and row_cells[0].text == row_cells[1].text:
+            row_cells = (row_cells[0], row_cells[2])
+
         if len(row_cells) != 2:
             print_('ERROR', f'table row has too many cells {doc_index=} {row_index=} {len(row_cells)=}')
-            return None
+            breakpoint()
+            continue
         
-        # cell0_style = row_cells[0].paragraphs[0].style.style_id
         cell1_style = row_cells[1].paragraphs[0].style.style_id    
         # We're only interested in the styles in the 2nd column - the 1st column is always OPM-conclusion
         if row_index == 0:
-            if cell1_style != 'OPM-conclusion':
-                print_('ERROR', f'table first row should be style: OPM-conclusion {doc_index=} {row_index=} {cell1_style=}')
-                return None
             # In Word, this first row is a single merged cell, but it comes through to here as two duplicate cells
             print_(f'table-{cell1_style}', f'{row_cells[1].paragraphs[0].text}')
         else:
-            print_(f'table-{cell1_style}', f'{row_cells[0].paragraphs[0].text} | {row_cells[1].paragraphs[0].text}')
+            for i, paragraph in enumerate(row_cells[1].paragraphs):
+                if i == 0:
+                    print_(f'table-{cell1_style}', f'{row_cells[0].text} | [1] {paragraph.text}')
+                else:
+                    indent = 0
+                    if paragraph.style.style_id.startswith('OPM-level'):
+                        indent = int(paragraph.style.style_id.split('OPM-level')[1])
+                    print_('table-cell-continued', f'  | [{indent}] ' + '    ' * (indent - 1) + f'{paragraph.text}')
+
+    # print_('table-end', '')
+
+def process_nonrule_table(table, doc_index):
+    for row_index, r in enumerate(table.rows):
+        row_cells = tuple(c for c in r.cells)
+        print_('nonrule-table', ' | '.join((cell.text for cell in row_cells)))
 
 document = docx.Document(input_filepath)
 for doc_index, doc_object in enumerate(document.iter_inner_content()):
     if isinstance(doc_object, docx.text.paragraph.Paragraph):
         process_paragraph(doc_object, doc_index)
     elif isinstance(doc_object, docx.table.Table):
-        parse_table(doc_object, doc_index)
-        print('\n')
+        process_table(doc_object, doc_index)
     else:
         print_('ERROR', f'Unhandled doc object: {type(doc_object)}')
 
